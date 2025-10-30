16bbaf066c1d0149f8582c4958f12f73b31337c5
Ivan Dolgov <wanttobox@gmail.com>
2025-10-30 09:40:32 +0300

    set handlers

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -56,7 +56,7 @@ func summaryMetrics(storage *MemStorage) http.HandlerFunc {
 			response.WriteString(fmt.Sprintf("%s: %v\n", key, value))
 		}
 
-		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.Write([]byte(response.String()))
 	}
 }

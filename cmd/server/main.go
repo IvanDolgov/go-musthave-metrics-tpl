5db2831488698cb62991ba3ec1fab41f8ba10e98
Ivan Dolgov <wanttobox@gmail.com>
2025-11-01 11:18:42 +0300

    middleware logger to internal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/IvanDolgov/go-musthave-metrics-tpl/internal/compress"
 	"github.com/IvanDolgov/go-musthave-metrics-tpl/internal/logger"
+	"github.com/IvanDolgov/go-musthave-metrics-tpl/internal/middleware"
 	"github.com/go-chi/chi/v5"
 	"go.uber.org/zap"
 )
@@ -37,7 +38,7 @@ func run(cfg Config) error {
 	router := chi.NewRouter()
 
 	// Добавляем middleware логирования для всех маршрутов
-	router.Use(withLogging)
+	router.Use(middleware.WithLogging)
 
 	// Добавляем middleware сжатия gzip для всех маршрутов
 	router.Use(withGzip)
@@ -179,36 +180,6 @@ func buildServerAddress(server, port string) string {
 	return server + ":" + port
 }
 
-// withLogging добавляет логирование для всех запросов и ответов
-func withLogging(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// засекаем время начала обработки запроса
-		start := time.Now()
-
-		// Создаем кастомный ResponseWriter для перехвата статуса и размера ответа
-		wrapped := &responseWriter{
-			ResponseWriter: w,
-			statusCode:     http.StatusOK,
-		}
-
-		// Обслуживаем запрос
-		h.ServeHTTP(wrapped, r)
-
-		// Вычисляем продолжительность выполнения
-		duration := time.Since(start)
-
-		// Логируем сведения о запросе и ответе на уровне Info
-		logger.Log.Info("HTTP request processed",
-			zap.String("uri", r.RequestURI),
-			zap.String("method", r.Method),
-			zap.String("remote_addr", r.RemoteAddr),
-			zap.Int("status_code", wrapped.statusCode),
-			zap.Int("response_size", wrapped.responseSize),
-			zap.Duration("duration", duration),
-		)
-	})
-}
-
 // withGzip добавляет сжатие
 func withGzip(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -234,39 +205,4 @@ func withGzip(h http.Handler) http.Handler {
 
 		h.ServeHTTP(ow, r)
 	})
-}
-
-// responseWriter обертка для http.ResponseWriter для перехвата статуса и размера ответа
-type responseWriter struct {
-	http.ResponseWriter
-	statusCode   int
-	responseSize int
-	wroteHeader  bool
-}
-
-func (rw *responseWriter) WriteHeader(code int) {
-	if !rw.wroteHeader {
-		rw.statusCode = code
-		rw.ResponseWriter.WriteHeader(code)
-		rw.wroteHeader = true
-	}
-}
-
-func (rw *responseWriter) Write(b []byte) (int, error) {
-	if !rw.wroteHeader {
-		rw.WriteHeader(http.StatusOK)
-	}
-	size, err := rw.ResponseWriter.Write(b)
-	rw.responseSize += size
-	return size, err
-}
-
-// Status возвращает статус код ответа
-func (rw *responseWriter) Status() int {
-	return rw.statusCode
-}
-
-// Size возвращает размер ответа
-func (rw *responseWriter) Size() int {
-	return rw.responseSize
 }
